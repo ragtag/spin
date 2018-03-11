e34d2d1b526b2c46a868c630e205fad5be295543
Ragnar Brynjulfsson <me@ragnarb.com>
2018-03-12 00:12:40 +0100

    Fixed type. Added notification on touchpage/nipple toggle.

--- toggle_touchpad.sh
+++ toggle_touchpad.sh
@@ -3,11 +3,13 @@
 NIPPLE="TPPS/2 IBM TrackPoint"
 TOUCHPAD="SynPS/2 Synaptics TouchPad"
 if `xinput --list "$TOUCHPAD" | grep --q disabled`; then
+    notify-send "Enabling touchpad and nipple"
     echo "Enabling $TOUCHPAD";
     xinput enable "$TOUCHPAD"
     echo "Enabling $NIPPLE"
     xinput enable "$NIPPLE"
 else
+    notify-send "Disabling touchpad and nipple"
     echo "Disabling $TOUCHPAD";
     xinput disable "$TOUCHPAD"
     echo "Disabling $NIPPLE"
